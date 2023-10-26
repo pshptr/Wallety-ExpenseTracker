4a812301545b5ef9c2db3029790f109470573335
PiotrCoder-creator <petia3711@gmail.com>
2023-10-27 01:09:08 +0300

    first commit

---
+++ Wallety/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Wallety
+//
+//  Created by Petia on 27.10.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Wallety/WalletyApp.swift
@@ -0,0 +1,17 @@
+//
+//  WalletyApp.swift
+//  Wallety
+//
+//  Created by Petia on 27.10.23.
+//
+
+import SwiftUI
+
+@main
+struct WalletyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
